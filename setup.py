c661aef6d839a23d18bb9cc9e64c29658959b296
Ilya Novak <ilyanovak@gmail.com>
2020-11-10 21:24:55 -0500

    first change

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+REQUIRES = [
+    "numpy",
+    "pandas"
+]
+
+setuptools.setup(
+    name='ilya test'
+    version='0.0.1'
+    author='inovak'
+    install_requires=REQUIRES
+)
